0c81b4d40566eb6e80895f8b148097e7a521d807
Onserioo <joshuaomirera@gmail.com>
2024-04-08 09:49:07 +0300

    .

--- src/main/java/com/solar_safari/main/MainApplication.java
+++ src/main/java/com/solar_safari/main/MainApplication.java
@@ -1,16 +1,16 @@
+package com.solar_safari.main;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
         StackPane root = new StackPane();
-        root.getChildren().add(label);
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 640, 480);
         primaryStage.setTitle("Solar System Safari");
         primaryStage.setScene(scene);
         primaryStage.show();
